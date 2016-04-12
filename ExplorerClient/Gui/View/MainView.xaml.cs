aab92aa66e76f72de31dd993380c4ad07d26caf2
Макс <q_man007@outlook.com>
2016-04-12 17:14:29 +0300

    Отмена MVVM. Начало разработки интерфеса используя Code Behind

---
+++ ExplorerClient/Gui/View/MainView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using ExplorerClient.Core;
+
+namespace ExplorerClient.Gui.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            Client.Connect("localhost", 3000);
+        }
+
+        private void MainViewWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            new LoginView().ShowDialog();
+        }
+    }
+}
